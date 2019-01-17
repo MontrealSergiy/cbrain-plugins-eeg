a8d2a7fe2a416c565887a34d450ad91fa26c643d
armin.ytaheri@gmail.com <armin.ytaheri@gmail.com>
2019-01-17 12:26:53 -0500

    [ADD] MOD File viewer and MOD file collection model

---
+++ userfiles/mod_collection/mod_collection.rb
@@ -0,0 +1,31 @@
+#
+# CBRAIN Project
+#
+# Copyright (C) 2008-2012
+# The Royal Institution for the Advancement of Learning
+# McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+class ModCollection < FileCollection
+  Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
+
+  has_viewer :name => "MOD Viewer", :partial => :mod_viewer
+
+  def self.pretty_type #:nodoc:
+    "MOD Collection"
+  end
+end
